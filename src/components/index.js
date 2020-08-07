86ab799f6fffe24e11162196ce0d55fe08753d61
Boro23 <404slimboysavage@gmail.com>
2020-08-07 17:47:12 +0530

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+//take all the components charts, cards and countrypicker and export it 
+export {default as Cards} from './Cards/Cards';
+export {default as Chart} from './Chart/Chart';
+export {default as CountryPicker} from './CountryPicker/CountryPicker';
